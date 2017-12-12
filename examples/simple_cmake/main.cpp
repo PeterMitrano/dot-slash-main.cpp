61a30057d63838857c0eaf5c772f6e004be909d0
Peter Mitrano <mitranopeter@gmail.com>
2017-12-11 20:44:18 -0500

    fancy printing in simple cmake example

--- examples/simple_cmake/main.cpp
+++ examples/simple_cmake/main.cpp
@@ -1,17 +1,47 @@
 #ifndef BASH
-echo "INSTALLING DEPENDENCIES..."
+echo -e "\033[33mINSTALLING DEPENDENCIES...\033[0m"
+echo ""
 sudo apt-get install cmake g++
-echo "RUNNING CMAKE..."
+echo ""
+echo -e "\033[33mRUNNING CMAKE...\033[0m"
+echo ""
 mkdir build
 cd build
 cmake ..
 make
-echo "RUNNING MAIN..."
-./main
+echo ""
+echo -e "\033[33mRUNNING MAIN...\033[0m"
+echo ""
+./main $@
 exit
 #endif
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
-int main(int argc, char **argv) {
-  std::cout << "hahahahah!" << std::endl;
+
+#include "sqrt.h"
+
+int main(int argc, char **argv)
+{
+  if (argc != 2) {
+    std::cout << "USAGE: sqrt number"
+              << std::endl
+              << std::endl
+              << "EXAMPLE: sqrt 64"
+              << std::endl;
+    return 0;
+  }
+
+  char *end;
+  double number = strtod(argv[1], &end);
+
+  if (end == argv[1] || *end != '\0')
+  {
+    std::cout << "Invalid characters: [" << end << "]" << std::endl;
+    return 0;
+  }
+
+  std::cout << sqrt(number) << std::endl;
+
   return 0;
 }
