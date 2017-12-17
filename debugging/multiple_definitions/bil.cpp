5340715469fe1cfcf1019dc900db241c23b17eae
Peter Mitrano <mitranopeter@gmail.com>
2017-12-16 22:39:01 -0500

    multiple definition example

---
+++ debugging/multiple_definitions/bil.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "bar.h"
+
+void func2() {
+  std::cout << x <<  " = x" << std::endl;
+}
---
+++ debugging/multiple_definitions/main.cpp
@@ -0,0 +1,8 @@
+#include "lib.h"
+#include "bil.h"
+
+int main(int argc, char **argv) {
+  func();
+  func2();
+  return 0;
+}
